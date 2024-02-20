d1ebfd25816e07accfaa1879335b2017052f833d
marsyaaurel08 <sefiramarsya@gmail.com>
2024-02-20 08:35:47 +0700

    P2_Percobaan1_Pertanyaan4_18

--- src/Pertemuan2/Buku18.java
+++ src/Pertemuan2/Buku18.java
@@ -1,24 +1,29 @@
 package Pertemuan2;
 
 public class Buku18 {
-    
+
     String judul, pengarang;
     int halaman, stok, harga;
 
     void tampilInformasi() {
         System.out.println("Judul: " + judul);
-        System.out.println("Pengarang: "+ pengarang);
-        System.out.println("Jumlah Halaman: "+ halaman);
-        System.out.println("Sisa stok: "+ stok);
-        System.out.println("Harga: Rp "+ harga);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah Halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
     }
-    void terjual(int jml){
-        stok-=jml;
+
+    void terjual(int jml) {
+        if (stok > 0){
+            stok -= jml;}
     }
-    void restock(int jml){
-        stok+=jml;
+
+    void restock(int jml) {
+        stok += jml;
     }
-    void gantiHarga(int hrg){
-        harga=hrg;
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
     }
+   
 }
