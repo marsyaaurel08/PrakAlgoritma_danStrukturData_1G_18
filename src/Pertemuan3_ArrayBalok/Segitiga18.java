d8614a25f9544aee65fccc349470a815eab1f509
marsyaaurel08 <sefiramarsya@gmail.com>
2024-03-03 22:01:49 +0700

    P3_Percobaan3_Pertanyaan2_18

---
+++ src/Pertemuan3_ArrayBalok/Segitiga18.java
@@ -0,0 +1,12 @@
+package Pertemuan3_ArrayBalok;
+
+public class Segitiga18 {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga18 (int a, int t){
+        alas = a;
+        tinggi = t;
+    }
+    
+}
