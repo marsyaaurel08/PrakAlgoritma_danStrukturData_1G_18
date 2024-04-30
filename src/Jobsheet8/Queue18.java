648931d10b9c2174bf5d385d54208bf96b6d26b0
marsyaaurel08 <sefiramarsya@gmail.com>
2024-04-30 18:51:23 +0700

    .

--- src/Jobsheet8/Queue18.java
+++ src/Jobsheet8/Queue18.java
@@ -56,7 +56,7 @@ public class Queue18 {
             System.out.println("Queue masih kosong");
         }
     }
-    public void Enqueue (int dt)throws Exception{
+    public void Enqueue (int dt){
         if (IsFull()){
             System.out.println("Queue sudah penuh!");
         }else{
@@ -72,7 +72,6 @@ public class Queue18 {
             data[rear] = dt;
             size++;
         }
-        System.exit(0);
     }
     public int Dequeue(){
         int dt=0;
@@ -91,7 +90,6 @@ public class Queue18 {
                 }
             }
         }
-        System.exit(0);
         return dt;
     }
 }
