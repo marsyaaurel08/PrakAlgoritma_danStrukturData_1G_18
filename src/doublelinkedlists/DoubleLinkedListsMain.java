43d779fbb7c863d5e42cb334506c468248242ac6
marsyaaurel08 <sefiramarsya@gmail.com>
2024-05-14 09:44:21 +0700

    P12_Percobaan1_18

---
+++ src/doublelinkedlists/DoubleLinkedListsMain.java
@@ -0,0 +1,24 @@
+package doublelinkedlists;
+
+public class DoubleLinkedListsMain {
+    public static void main(String[] args) throws Exception {
+        doublelinkedlists dll = new doublelinkedlists();
+        dll.print();
+        System.out.println("Size : "+dll.size());
+        System.out.println("===============================================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size : "+dll.size());
+        System.out.println("===============================================");
+        dll.add(40,1);
+        dll.print();
+        System.out.println("Size : "+dll.size());
+        System.out.println("===============================================");
+        dll.clear();
+        dll.print();
+        System.out.println("Size : "+dll.size());
+
+    }
+}
