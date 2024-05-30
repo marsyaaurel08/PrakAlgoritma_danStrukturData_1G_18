cabddb2684b481045964aa0eb419406eeb1576e1
marsyaaurel08 <sefiramarsya@gmail.com>
2024-05-30 19:46:27 +0700

    P13_Percoban1_18

---
+++ src/Jobsheet11/BinaryTree18.java
@@ -0,0 +1,164 @@
+package Jobsheet11;
+
+public class BinaryTree18 {
+    Node18 root;
+
+    public BinaryTree18() {
+        root = null;
+    }
+
+    boolean isEmpty() {
+        return root == null;
+    }
+
+    void add(int data) {
+        if (isEmpty()) { // tree is empty
+            root = new Node18(data);
+        } else {
+            Node18 current = root;
+            while (true) {
+                if (data < current.data) {
+                    if (current.left == null) {
+                        current.left = new Node18(data);
+                        break;
+                    } else {
+                        current = current.left;
+                    }
+                } else if (data > current.data) {
+                    if (current.right == null) {
+                        current.right = new Node18(data);
+                        break;
+                    } else {
+                        current = current.right;
+                    }
+                } else { // data already exists
+                    break;
+                }
+            }
+        }
+    }
+
+    boolean find(int data) {
+        Node18 current = root;
+        while (current != null) {
+            if (data == current.data) {
+                return true;
+            } else if (data < current.data) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return false;
+    }
+
+    void traversePreOrder(Node18 node) {
+        if (node != null) {
+            System.out.print(" " + node.data);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    void traversePostOrder(Node18 node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.data);
+        }
+    }
+
+    void traverseInOrder(Node18 node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.data);
+            traverseInOrder(node.right);
+        }
+    }
+
+    Node18 getSuccessor(Node18 del) {
+        Node18 successor = del.right;
+        Node18 successorParent = del;
+        while (successor.left != null) {
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if (successor != del.right) {
+            successorParent.left = successor.right;
+            successor.right = del.right;
+        }
+        return successor;
+    }
+
+    void delete(int data) {
+        if (isEmpty()) {
+            System.out.print("Tree is empty!");
+            return;
+        }
+
+        Node18 parent = root;
+        Node18 current = root;
+        boolean isLeftChild = false;
+
+        while (current != null) {
+            if (data == current.data) {
+                break;
+            } else if (data < current.data) {
+                parent = current;
+                current = current.left;
+                isLeftChild = true;
+            } else {
+                parent = current;
+                current = current.right;
+                isLeftChild = false;
+            }
+        }
+
+        if (current == null) {
+            System.out.println("Couldn't find data!");
+            return;
+        }
+
+        if (current.left == null && current.right == null) {
+            if (current == root) {
+                root = null;
+            } else {
+                if (isLeftChild) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            }
+        } else if (current.left == null) {
+            if (current == root) {
+                root = current.right;
+            } else {
+                if (isLeftChild) {
+                    parent.left = current.right;
+                } else {
+                    parent.right = current.right;
+                }
+            }
+        } else if (current.right == null) {
+            if (current == root) {
+                root = current.left;
+            } else {
+                if (isLeftChild) {
+                    parent.left = current.left;
+                } else {
+                    parent.right = current.left;
+                }
+            }
+        } else {
+            Node18 successor = getSuccessor(current);
+            if (current == root) {
+                root = successor;
+            } else if (isLeftChild) {
+                parent.left = successor;
+            } else {
+                parent.right = successor;
+            }
+            successor.left = current.left;
+        }
+    }
+}
---
+++ src/Jobsheet11/Node18.java
@@ -0,0 +1,16 @@
+package Jobsheet11;
+
+public class Node18 {
+    int data;
+    Node18 left;
+    Node18 right;
+
+    public Node18(){
+
+    }
+    public Node18(int data){
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
