d0c99ca9779372ff5fd61c794623f38c2c00721f
marsyaaurel08 <sefiramarsya@gmail.com>
2024-05-30 20:40:04 +0700

    P13_Percobaan2_18

---
+++ src/Jobsheet11/BinaryTreeArray18.java
@@ -0,0 +1,21 @@
+package Jobsheet11;
+
+public class BinaryTreeArray18 {
+    int[] data;
+    int idxLast;
+
+    public BinaryTreeArray18(){
+        data = new int[10];
+    }
+    void populateData(int data[], int idxLast){
+        this.data = data;
+        this.idxLast = idxLast;
+    }
+    void traverseInOrder(int idxStart){
+        if(idxStart<=idxLast){
+            traverseInOrder(2*idxStart+1);
+            System.out.print(data[idxStart]+" ");
+            traverseInOrder(2*idxStart+2);
+        }
+    }
+}
