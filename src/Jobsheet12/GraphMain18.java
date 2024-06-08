45e88ec1b0610a565105dbb254c282a7206ef34a
marsyaaurel08 <sefiramarsya@gmail.com>
2024-06-08 20:54:41 +0700

    P15_Percobaan1_Pertanyaan5_18

--- src/Jobsheet12/GraphMain18.java
+++ src/Jobsheet12/GraphMain18.java
@@ -1,5 +1,7 @@
 package Jobsheet12;
 
+import java.util.Scanner;
+
 public class GraphMain18 {
     public static void main(String[] args) {
         Graph18 gedung = new Graph18(6);
@@ -16,6 +18,26 @@ public class GraphMain18 {
             
             gedung.removeEdge(1, 3);
             gedung.printGraph();
+            
+            Scanner sc18 = new Scanner(System.in);
+            
+            System.out.print("Masukkan gedung asal: ");
+            int asal = sc18.nextInt();
+            System.out.print("Masukkan gedung tujuan: ");
+            int tujuan = sc18.nextInt();
+        
+            try {
+                if (gedung.cekKetetanggan(asal, tujuan)) {
+                    System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " bertetangga");
+                } else {
+                    System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " tidak bertetangga");
+                }
+            } catch (Exception e) {
+                System.err.println("Terjadi kesalahan: " + e.getMessage());
+            }
+            
+            
+            sc18.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
